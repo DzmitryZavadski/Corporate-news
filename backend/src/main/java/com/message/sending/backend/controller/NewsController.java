b7a408290b35eb3c6f6f38f7e28fa2a3c8e1ad39
DzmitryZavadski <dzmitry.zavadski@innowise.com>
2026-05-22 15:04:52 +0200

    Big commit

--- backend/src/main/java/com/message/sending/backend/controller/NewsController.java
+++ backend/src/main/java/com/message/sending/backend/controller/NewsController.java
@@ -12,6 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/news")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class NewsController {
     private final NewsService newsService;
 
